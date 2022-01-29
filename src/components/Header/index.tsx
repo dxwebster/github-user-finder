c3deb8c63828cdad731271d2773c114ecbd83fcb
dxwebster <adrianalimafm@gmail.com>
2022-01-29 19:28:08 -0300

    feat: create nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,69 +2,25 @@ import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector, RootStateOrAny } from 'react-redux';
 
-import { Container, HeaderRight, HeaderLeft, Avatar, TooltipHeader } from './styles';
-
+import { Container, LogoContent, BackButton } from './styles';
 import SvgArrowBack from '../../assets/SvgArrowBack';
-import SvgNotification from '../../assets/SvgNotification';
-import SvgMessage from '../../assets/SvgMessage';
-import SvgProfile from '../../assets/SvgProfile';
-// import logoImg from '../../assets/logo.svg';
-// import avatarImg from '../../assets/avatar.png';
+import SvgLogo from '../../assets/SvgLogo';
 
 export default function Header() {
-  const { name } = useSelector((state: RootStateOrAny) => state.user);
-  const [fixed, setFixed] = useState(null);
-  const { pathname } = useLocation();
-
   return (
-    <>
-      <Container>
-        <HeaderLeft>
-          <ul>
-            <li>
-              <Link to="/">
-                <span>Voltar</span>
-              </Link>
-            </li>
-          </ul>
-        </HeaderLeft>
+    <Container>
+      <main>
+        <BackButton>
+          <Link to="/">
+            <SvgArrowBack />
+            <span>Voltar</span>
+          </Link>
+        </BackButton>
 
-        <HeaderRight>
-          <ul>
-            <li>
-              <SvgNotification />
-            </li>
-            <li>
-              <SvgMessage />
-            </li>
-          </ul>
-
-          <main
-            onClick={() => {
-              setFixed(!fixed);
-            }}
-          >
-            <p>{name}</p>
-          </main>
-        </HeaderRight>
-
-        <TooltipHeader fixed={fixed}>
-          <ul>
-            <li>
-              <SvgProfile /> Meu perfil
-            </li>
-            <li>
-              <SvgProfile /> Meus cursos
-            </li>
-            <li>
-              <SvgProfile /> Configurações
-            </li>
-            <li>
-              <SvgProfile /> Sair
-            </li>
-          </ul>
-        </TooltipHeader>
-      </Container>
-    </>
+        <LogoContent>
+          <SvgLogo />
+        </LogoContent>
+      </main>
+    </Container>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,55 +3,48 @@ import { darken } from 'polished';
 
 export const Container = styled.header`
   width: 100%;
-  height: 50px;
+  height: 9rem;
 
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  background: ${(props) => props.theme.title};
-  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);
-  padding: 10px 60px;
-  color: ${(props) => props.theme.darkGrey};
-
-  position: relative;
-`;
-export const HeaderLeft = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 14px;
-  font-weight: bold;
 
-  ul {
+  background: ${(props) => props.theme.darkGrey};
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.25);
+  color: ${(props) => props.theme.lightGrey};
+
+  main {
+    width: 70%;
     display: flex;
-    list-style: none;
+    align-items: center;
+    justify-content: center;
+    position: relative;
 
-    li {
-      display: flex;
-      align-items: center;
-      font-size: 13px;
-      font-weight: bold;
-
-      & + li {
-        margin-left: 30px;
-      }
-
-      a {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: ${(props) => props.theme.darkGrey};
-
-        svg {
-          margin-right: 8px;
-        }
-      }
+    svg {
+      width: 80%;
     }
   }
 `;
 
-export const HeaderRight = styled.div`
+export const BackButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  left: 0;
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: bold;
+    gap: 1rem;
+  }
+`;
+
+export const LogoContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -82,7 +75,7 @@ export const HeaderRight = styled.div`
     }
   }
 
-  main {
+  div {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -92,77 +85,5 @@ export const HeaderRight = styled.div`
     p {
       margin-left: 20px;
     }
-  }
-`;
-
-export const Avatar = styled.img`
-  height: 34px;
-  width: 34px;
-  border-radius: 50%;
-  margin: 0 0 0 15px;
-  cursor: pointer;
-`;
-
-interface TooltipHeaderProps {
-  fixed: string;
-}
-
-export const TooltipHeader = styled.div<TooltipHeaderProps>`
-  width: 200px;
-  font-size: 14px;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  background: ${(props) => props.theme.title};
-  box-shadow: rgba(0, 0, 0, 0.6) 0px 5px 20px;
-  color: ${(props) => props.theme.darkGrey};
-  border-radius: 5px;
-
-  position: absolute;
-  z-index: 9999;
-  right: 10px;
-  top: 70px;
-
-  visibility: ${(props) => (props.fixed ? 'visible' : 'hidden')};
-  opacity: ${(props) => (props.fixed ? '1' : '0')};
-
-  ul {
-    width: 100%;
-
-    li {
-      padding: 7px 10px;
-      cursor: pointer;
-      display: flex;
-      align-items: center;
-
-      &:hover {
-        background-color: ${(props) => darken(0.2, props.theme.title)};
-      }
-
-      svg {
-        margin-right: 5px;
-      }
-    }
-  }
-
-  &:before {
-    content: '';
-    display: block;
-    float: left;
-
-    width: 20px;
-    height: 20px;
-
-    margin: 5px 45px 5px 20px;
-    transform: rotate(45deg);
-    background-color: ${(props) => props.theme.title};
-
-    position: absolute;
-
-    right: -12px;
-    top: -10px;
-    z-index: -1;
   }
 `;
